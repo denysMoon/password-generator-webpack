33e190f459fcb22ac254b5f49eda784a87f9d39c
denysMoon <denysulvanskyi@gmail.com>
2021-08-10 00:01:14 +0300

    add password generator

--- src/index.js
+++ src/index.js
@@ -3,3 +3,27 @@ import './style/style.css'
 const generateButton = document.querySelector('#generate-button')
 const copyButton = document.querySelector('#copy-button')
 const input = document.querySelector('input')
+
+const writePassword = () =>{
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const passwordLength = 12
+    let password = ''
+
+    for(let i = 0; i<passwordLength; i++){
+        const randomNumber = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNumber, randomNumber +1)
+    }
+
+    input.value = password
+}
+
+const copyPassword = () =>{
+    const password = document.querySelector('input')
+    password.select()
+    document.execCommand("copy")
+    console.log(password.value)
+}
+
+generateButton.addEventListener('click', writePassword)
+copyButton.addEventListener('click', copyPassword)
+
